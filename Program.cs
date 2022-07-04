9a78a0b7a60b69005f6f60ecdd616769af85f242
edwink.ronoh@gmail.com <edwink.ronoh@gmail.com>
2022-07-04 23:02:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HackerRank_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i=1; i<=10;i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+        }
+    }
+}
